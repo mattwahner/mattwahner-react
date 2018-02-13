e7ee028d1e96ac41dea1306b607e5b51fe15cdff
Matthew Wahner <mattwahner@gmail.com>
2018-02-13 00:57:05 -0600

    Add PWA support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import BoardPage from "./components/BoardPage";
+import HomePage from "./components/HomePage";
 import './app.css';
 
 const App = () => (
-    <div>
+    <div className="App">
+        <Route path="/" exact component={HomePage} />
         <Route path="/board" exact component={BoardPage} />
     </div>
 );
